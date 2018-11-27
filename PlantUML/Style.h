399c2449719f4d94b7240df924e6b7525aaddf41
Manny-g <mannyguzman88@gmail.com>
2018-11-27 08:39:36 -0600

    Initial commit of District Prices problem, description needs to be updated

---
+++ PlantUML/Style.h
@@ -0,0 +1,44 @@
+'skinparam DefaultFontName "Roboto"
+'skinparam BackgroundColor transparent
+'skinparam Shadowing false
+'skinparam Handwritten true
+
+skinparam Activity {
+	ArrowColor		#6495ED
+	ArrowFontSize	10
+	'BarColor		#F0A0A0
+	BorderColor		#303030
+	BackgroundColor	#FFEFD5
+	'FontSize		20
+	'FontColor		#Red
+	'FontStyle		bold
+}
+
+skinparam Package {
+	StartColor					#303030
+	EndColor					#303030
+	BackgroundColor				#D3D3D3
+	BackgroundColor<<Failure>>	#FEA0A0
+	BackgroundColor<<Success>>	#A0FEA0
+	BorderColor					#303030
+	ArrowColor					#4169E1
+	FontStyle					bold
+	'FontColor					#Red
+}
+
+SkinParam Class {
+    HeaderBackgroundColor	#D3D3D3
+    ArrowColor				#6495ED
+    BorderColor				#303030
+    BackgroundColor			#FFEFD5
+    FontSize				10
+    'FontStyle				Bold
+    'FontColor				DARK
+	'AttributeFontColor		RED
+}
+
+skinparam Note {
+	BorderColor		#303030
+	BackgroundColor	#CEEEFE
+	FontSize		10
+}
